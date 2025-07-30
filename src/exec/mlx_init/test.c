70a284a950128d429ec32046af339f9795e15346
lylfergu <lylou.ferg@gmail.com>
2025-07-30 02:22:24 +0200

    main initiale

--- src/exec/mlx_init/test.c
+++ src/exec/mlx_init/test.c
@@ -3,6 +3,22 @@
 
 #include "cub3d.h"
 
+int	main(void)
+{
+	t_global	data;
+
+	initialize_structures(&data);
+//	if (av && av[1])
+//		parse_map_root(&data, av[1]);
+	if (init_mlx(&data) == FAILURE)
+	{
+		write(2, "Err: MLX initialization failed.\n", 31);
+		return (1);
+	}
+	mlx_loop(data.mlx_ptr);
+	write(STDIN_FILENO, "Game exited successfully.\n", 26);
+	return (0);
+}
 /*int	main(int ac, char **av, char **env)
 {
 	t_global	data;
