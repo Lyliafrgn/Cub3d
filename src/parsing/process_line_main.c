7576d799f83edf674d41e4fc30028d1401f6804a
Olivier Filloux <ofilloux@student.42barcelona.com>
2025-10-08 11:32:30 +0200

    same, mais sans bug @

--- src/parsing/process_line_main.c
+++ src/parsing/process_line_main.c
@@ -6,7 +6,7 @@
 /*   By: ofilloux <ofilloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 16:49:59 by ofilloux          #+#    #+#             */
-/*   Updated: 2025/10/08 11:13:09 by ofilloux         ###   ########.fr       */
+/*   Updated: 2025/10/08 11:30:48 by ofilloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 bool	invalid_line(char *line)
 {
+	if (!line)
+		return (false);
+	while (*line && *line == ' ')
+		line++;
 	if (!ft_strnstr(line, "NO ", 3) && !ft_strnstr(line, "SO ", 3) \
 		&& !ft_strnstr(line, "WE ", 3) && !ft_strnstr(line, "EA ", 3) \
 		&& !ft_strnstr(line, "F ", 2) && !ft_strnstr(line, "C ", 2) \
-		&& !ft_strnstr(line, "0", 1) && !ft_strnstr(line, "1", 1))
+		&& !ft_strnstr(line, "0", 1) && !ft_strnstr(line, "1", 1) \
+		&& !ft_strnstr(line, "\n", 1))
 		return (true);
 	return (false);
 }
@@ -30,7 +35,7 @@ int	process_line(t_global *data, t_map *map, char *line)
 	data->pars_sta.empty = line_is_only_spaces(line);
 	if (invalid_line(tmp))
 	{
-		err_msg("Invlid config\n");
+		err_msg("Invalid config\n");
 		free_resources(data);
 		ft_free((void **) &tmp);
 		return (EXIT_FAILURE);
